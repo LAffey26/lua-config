082b58c073a9a649ba8946be510a6aab92cf8252
LAffey26 <anercontae25@gmail.com>
2024-01-05 18:36:30 +0300

    add new keymaps

--- lua/config/core/keymaps.lua
+++ lua/config/core/keymaps.lua
@@ -7,8 +7,13 @@ local keymap = vim.keymap
 
 keymap.set('i', 'jk', '<ESC>', {desc = 'Exit insert mode'})
 
---Nvim-tree
+--tubs
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) 
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) 
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) 
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
 
+--Nvim-tree
 keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) 
 keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) 
 keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
@@ -16,17 +21,13 @@ keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file
 
 --Telescope
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
 keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+
 
 --Side keymaps
-
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 keymap.set('n', '<leader>w', ':w<CR>', {desc = 'Saving  file changes'})
 keymap.set('n', '<leader>q', ':q!<CR>', {desc = 'Exiting the nvim editor'})
-
-
-
 
 
 
